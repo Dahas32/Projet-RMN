d28329c7f99ae1a4b8c6dfc0b4eb85dc3a0d7d19
Dahas <theo.trossevin@gmail.com>
2023-01-30 09:45:48 +0100

    formattage

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-""" This program is made to read RMN treated output (after the fourier and modifying the phase) to find T1 (caracteristic time) and d (diffusion something) 
+""" This program is made to read RMN treated output (after the fourier and modifying
+    the phase) to find T1 (caracteristic time) and d (diffusion something)
     this file is made to take the I/O and to call each script."""
 
