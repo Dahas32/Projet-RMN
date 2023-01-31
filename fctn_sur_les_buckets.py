a19bb682b900536349b715d7b659c21a39c22f54
Hicha-m <68863445+Hicha-m@users.noreply.github.com>
2023-01-31 19:57:30 +0100

    make function noise_threshold  in test_fonction_buckets. it remove bucket according to a threshold depending on amplitude. file test to test it.

--- fctn_sur_les_buckets.py
+++ fctn_sur_les_buckets.py
@@ -37,3 +37,31 @@ def determination_des_buckets(data_list, taille_buckets_ppm):
             index_current_ppm += 1
 
     return buckets_list
+
+# this is the function that remove bucket above or below a certain threshold. it takes in entry data_list wich is the second element of the list return by
+# rmn.py : [[data_dim1,data_dim2,...]],  buckets_list : [buckets_dim1,bucket_dim2]   
+# and threshold is a integer given by the user.
+# this function return a list of bucket without bucket not required whith the shape buckets_filter_list = [buckets_dim1,bucket_dim2] where bucket_dim1
+# is a list of tuple (first,last) where first and last are two int indicating the index of the inf and sup boundaries of the bucket
+
+
+def noise_threshold(data_list, buckets_list, threshold):
+    number_of_dimension = len(data_list[0])
+    buckets_filter_list = []
+    
+    for i in range(number_of_dimension):
+        buckets_filter_list += [[]]
+        current_dimension = i
+
+        for bucket in buckets_list[current_dimension]:
+            
+            for y in range (bucket[0],bucket[1],1):
+                noise = False
+                valeur_I = data_list[0][current_dimension][y][1]
+                # to change if we want a specific threshold : up threshold and down threshold
+                if (valeur_I >= -threshold) and (threshold >= valeur_I):
+                    noise = True
+                    break
+            if noise == False: buckets_filter_list[current_dimension] += [bucket]
+      
+    return buckets_filter_list
