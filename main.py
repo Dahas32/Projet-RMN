1e2fc9ee815ce2a776b0ec0b39a9ad028589c2db
Dahas <theo.trossevin@gmail.com>
2023-02-01 13:42:19 +0100

    ajout afficher

--- main.py
+++ main.py
@@ -2,3 +2,18 @@
     the phase) to find T1 (caracteristic time) and d (diffusion something)
     this file is made to take the I/O and to call each script."""
 
+import rmn
+import matplotlib.pyplot as plt
+
+
+def main():
+    """main function"""
+    data = rmn.read_rmn(input("file path"))
+    for i in range(data[-1]):
+        rmn.afficher(data, i)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
