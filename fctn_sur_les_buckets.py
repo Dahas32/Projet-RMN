160d3811b042fe36308a0c2b0cbf9d1b47f4ec9f
Hicha-m <68863445+Hicha-m@users.noreply.github.com>
2023-02-13 19:50:20 +0100

    create function noise_automate add later test

--- fctn_sur_les_buckets.py
+++ fctn_sur_les_buckets.py
@@ -6,18 +6,19 @@ from math import floor
 
 
 def determination_des_buckets(data_list, taille_buckets_ppm):
-    """data_list=[[data_dim1,data_dim2,...]]"""
-    # this is the function that determine the buckets. it takes in argument data_list wich is the second element of the list return by
-    # rmn.py : data_list=[[data_dim1,data_dim2,...]] and taille_bucket is a float given by the user.
-    # this function return a list whith the shape bucket_list = [buckets_dim1,bucket_dim2] where bucket_dim1
-    # is a list of tuple (debut,fin) where debut and fin are two int indicating the index of the inf and sup boundaries of the bucket
+    """data_list=[[data_dim1,data_dim2,...]]
+    this is the function that determine the buckets. it takes in argument data_list wich is the second element of the list return by
+    rmn.py : data_list=[[data_dim1,data_dim2,...]] and taille_bucket is a float given by the user.
+    this function return a list whith the shape bucket_list = [buckets_dim1,bucket_dim2] where bucket_dim1
+    is a list of tuple (debut,fin) where debut and fin are two int indicating the index of the inf and sup boundaries of the bucket
+    """
 
     number_of_dimension = len(data_list)
     buckets_list = []
 
-    for i in range(number_of_dimension):
-        buckets_list += [[]]
-        current_dimension = i
+    for n in range(number_of_dimension):
+        buckets_list.append([])
+        current_dimension = n
         index_dernier_ppm = len(data_list[current_dimension]) - 1
         valeur_premier_ppm = data_list[current_dimension][0][0]
         valeur_dernier_ppm = data_list[current_dimension][-1][0]
@@ -40,27 +41,24 @@ def determination_des_buckets(data_list, taille_buckets_ppm):
 
 
 def calcul_des_integrales(filtered_bucket_list, data_list):
-    # this is the function that compute the buckets integrals. it takes in argument data_list wich is the second element of the list return by
-    # rmn.py : data_list=[[data_dim1,data_dim2,...]] and filtered_bucket_list wich is the list of the bucket without the noised ones.
-    # this function return a list whith the shape integral_list = [[integral_bucket1,integral_bucket2,...],[integral_bucket_dim2_1,integral_bucket_dim2_2...],...]
-    # where integral_bucket1 is a float.
-    # the index of the integral of a particular bucket is the same as the index of this bucket in _filtered_bucket_list
-
+    """this is the function that compute the buckets integrals. it takes in argument data_list wich is the second element of the list return by
+    rmn.py : data_list=[[data_dim1,data_dim2,...]] and filtered_bucket_list wich is the list of the bucket without the noised ones.
+    this function return a list whith the shape integral_list = [[integral_bucket1,integral_bucket2,...],[integral_bucket_dim2_1,integral_bucket_dim2_2...],...]
+    where integral_bucket1 is a float.
+    the index of the integral of a particular bucket is the same as the index of this bucket in _filtered_bucket_list
+    """
     number_of_dimension = len(filtered_bucket_list)
     integral_list = []
 
     for n in range(number_of_dimension):
-
         current_dimension = n
-        integral_list += [[]]
+        integral_list.append([])
 
         for bucket in filtered_bucket_list[current_dimension]:
-
             bucket_integral = 0
             number_of_rectangle = bucket[1] - bucket[0]
 
             for i in range(number_of_rectangle):
-
                 first_point_amplitude = data_list[current_dimension][bucket[0] + i][1]
                 second_point_amplitude = data_list[current_dimension][
                     bucket[0] + i + 1
@@ -77,36 +75,79 @@ def calcul_des_integrales(filtered_bucket_list, data_list):
     return integral_list
 
 
-a = (2, 3)
-print((a[1] - a[0]) + 1)
-
-# this is the function that remove bucket above or below a certain threshold. it takes in entry data_list wich is the second element of the list return by
-# rmn.py : [[data_dim1,data_dim2,...]],  buckets_list : [buckets_dim1,bucket_dim2]
-# and threshold is a integer given by the user.
-# this function return a list of bucket without bucket not required whith the shape buckets_filter_list = [buckets_dim1,bucket_dim2] where bucket_dim1
-# is a list of tuple (first,last) where first and last are two int indicating the index of the inf and sup boundaries of the bucket
-
-
 def noise_threshold(data_list, buckets_list, threshold):
-    """ data_list: [[data_dim1,data_dim2,...]], buckets_list: [buckets_dim1,bucket_dim2,...] , threshold: int
+    """data_list: [[data_dim1],[data_dim2],...]], buckets_list: [[buckets_dim1],[bucket_dim2],...] , threshold: int
+        return buckets_filter_list : [[(first,last),...],[buckets_filter_list_dim2]...]
+
+    this is the function that remove bucket above or below a certain threshold. it takes in entry data_list wich is the second element of the list return by
+    rmn.py : [[data_dim1,data_dim2,...]],  buckets_list : [buckets_dim1,bucket_dim2]
+    and threshold is a integer given by the user.
+    this function return a list of bucket without bucket not required whith the shape buckets_filter_list = [buckets_dim1,bucket_dim2] where bucket_dim1
+    is a list of tuple (first,last) where first and last are two int indicating the index of the inf and sup boundaries of the bucket
     """
     number_of_dimension = len(data_list)
     buckets_filter_list = []
 
-    for i in range(number_of_dimension):
-        buckets_filter_list += [[]]
-        current_dimension = i
+    for n in range(number_of_dimension):
+        buckets_filter_list.append([])
+        current_dimension = n
 
         for bucket in buckets_list[current_dimension]:
-
-            for y in range(bucket[0], bucket[1], 1):
+            for i in range(bucket[0], bucket[1], 1):
                 noise = False
-                valeur_I = data_list[current_dimension][y][1]
+                valeur_I = data_list[current_dimension][i][1]
                 # to change if we want a specific threshold : up threshold and down threshold
                 if (valeur_I >= -threshold) and (threshold >= valeur_I):
                     noise = True
                     break
-            if not noise:
-                buckets_filter_list[current_dimension] += [bucket]
+            if noise == False:
+                buckets_filter_list[current_dimension].append(bucket)
+
+    return buckets_filter_list
+
+
+def noise_automate(data_list, buckets_list):
+    """data_list: [[data_dim1],[data_dim2],...]], buckets_list: [[buckets_dim1],[bucket_dim2],...]
+    return buckets_filter_list : [[(first,last),...],[buckets_filter_list_dim2]...]
+    """
+    number_of_dimension = len(data_list)
+    buckets_filter_list = []
+
+    for n in range(number_of_dimension):
+        buckets_filter_list.append([])
+        current_dimension = n
+        average_buckets = []
+        average_of_average_buckets = 0
+        # average abs of each bucket
+        for bucket in buckets_list[current_dimension]:
+            average_bucket = 0
+            for i in range(bucket[0], bucket[1], 1):
+                average_bucket += data_list[current_dimension][i][1]
+            average_bucket = abs(int((average_bucket // (bucket[1] - bucket[0]))))
+            average_buckets.append(average_bucket)
+            average_of_average_buckets += average_bucket
+
+        length_average_buckets = len(average_buckets)
+        average_of_average_buckets = int(
+            average_of_average_buckets // length_average_buckets
+        )
+
+        variance = 0
+        for i in range(length_average_buckets):
+            variance += (
+                data_list[current_dimension][i][1] - average_of_average_buckets
+            ) ** 2
+        variance = int(variance // length_average_buckets)
+        standard_deviation = int(variance ** (1 / 2))
+
+        for i in range(1, length_average_buckets - 1):
+            if (
+                average_buckets[i - 1] / average_buckets[i] < 2
+                and average_buckets[i + 1] / average_buckets[i] < 2
+                and average_buckets[i] > average_of_average_buckets - standard_deviation
+            ):
+                buckets_filter_list[current_dimension].append(
+                    buckets_list[current_dimension][i]
+                )
 
     return buckets_filter_list
--- test_fonction_buckets.py
+++ test_fonction_buckets.py
@@ -1,13 +1,15 @@
 """"ce programme contient les tests des fonctions contenus dans le fichier "fonctions_sur_les_buckets.py""" ""
 
-from fctn_sur_les_buckets import determination_des_buckets, noise_threshold
+from fctn_sur_les_buckets import (
+    determination_des_buckets,
+    calcul_des_integrales,
+    noise_threshold,
+    noise_automate,
+)
 
 
-# test determination_des_buckets
-
-
-def test_determination_buckets_1D_1():
-    liste_test1 = [[[(i, 0) for i in range(10)]]]
+def test_determination_buckets_1D_integer():
+    liste_test1 = [[(i, 0) for i in range(10)]]
     taille_bucket_ppm = 1
     assert determination_des_buckets(liste_test1, taille_bucket_ppm) == [
         [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)]
@@ -18,38 +20,21 @@ def test_determination_buckets_1D_1():
     ]
 
 
-def test_determination_buckets_1D_2():
-    liste_test2 = [[[(i, 0) for i in range(10)]]]
-    taille_bucket_ppm = 2
+def test_determination_buckets_1D_float():
+    liste_test1 = [[(i * 0.5, 0) for i in range(10)]]
+    liste_test2 = [[(i * 0.5, 0) for i in range(10, 0, -1)]]
+    taille_bucket_ppm = 0.5
+    assert determination_des_buckets(liste_test1, taille_bucket_ppm) == [
+        [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)]
+    ]
     assert determination_des_buckets(liste_test2, taille_bucket_ppm) == [
-        [(0, 2), (2, 4), (4, 6), (6, 8)]
-    ]
-
-
-def test_determination_buckets_1D_3():
-    liste_test3 = [[[(i * 0.5, 0) for i in range(10)]]]
-    taille_bucket_ppm = 0.5
-    assert determination_des_buckets(liste_test3, taille_bucket_ppm) == [
         [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)]
     ]
 
 
-def test_determination_buckets_1D_4():
-    liste_test3 = [[[(i * 0.5, 0) for i in range(0, 10, 1)]]]
-    liste_test3_1 = [[[(i * 0.5, 0) for i in range(10, 0, -1)]]]
-    taille_bucket_ppm = 0.5
-
-    assert determination_des_buckets(liste_test3, taille_bucket_ppm) == [
-        [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)]
-    ]
-    assert determination_des_buckets(liste_test3_1, taille_bucket_ppm) == [
-        [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)]
-    ]
-
-
-def test_determination_buckets_1D_5():
-    liste_test1 = [[[(i, 0) for i in range(-5, 17, 1)]]]
-    liste_test2 = [[[(i, 0) for i in range(17, -5, -1)]]]
+def test_determination_buckets_1D_list_reversed():
+    liste_test1 = [[(i, 0) for i in range(-5, 17, 1)]]
+    liste_test2 = [[(i, 0) for i in range(17, -5, -1)]]
     taille_bucket_ppm = 3
 
     assert determination_des_buckets(
@@ -57,40 +42,65 @@ def test_determination_buckets_1D_5():
     ) == determination_des_buckets(liste_test2, taille_bucket_ppm)
 
 
-def test_determination_buckets_2D_1():
-    liste_test4 = [
-        [[(i * 0.5, 0) for i in range(10)], [(i * 0.5, 0) for i in range(10)]]
+def test_determination_buckets_2D_integer():
+    liste_test1 = [[(i, 0) for i in range(10)], [(i, 0) for i in range(10)]]
+    taille_bucket_ppm = 1
+    assert determination_des_buckets(liste_test1, taille_bucket_ppm) == [
+        [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)],
+        [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)],
     ]
+    taille_bucket_ppm = 2
+    assert determination_des_buckets(liste_test1, taille_bucket_ppm) == [
+        [(0, 2), (2, 4), (4, 6), (6, 8)],
+        [(0, 2), (2, 4), (4, 6), (6, 8)],
+    ]
+
+
+def test_determination_buckets_2D_float():
+    liste_test1 = [[(i * 0.5, 0) for i in range(10)], [(i * 0.5, 0) for i in range(10)]]
     taille_bucket_ppm = 0.5
-    assert determination_des_buckets(liste_test4, taille_bucket_ppm) == [
+    assert determination_des_buckets(liste_test1, taille_bucket_ppm) == [
         [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)],
         [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)],
     ]
 
 
-def test_noise_threshold_1():
-    liste_test1 = [[[(x, (x ** 2) - 30) for x in range(-10, 12, 1)]]]
-    taille_bucket_ppm_1 = 2
-    det_1 = determination_des_buckets(liste_test1, taille_bucket_ppm_1)
+def test_determination_buckets_2D_list_reversed():
+    liste_test1 = [
+        [(i, 0) for i in range(-5, 17, 1)],
+        [(i, 0) for i in range(17, -5, -1)],
+    ]
+    liste_test2 = [
+        [(i, 0) for i in range(17, -5, -1)],
+        [(i, 0) for i in range(-5, 17, 1)],
+    ]
+    taille_bucket_ppm = 3
+
+    assert determination_des_buckets(
+        liste_test1, taille_bucket_ppm
+    ) == determination_des_buckets(liste_test2, taille_bucket_ppm)
+
+
+def test_noise_threshold_1D():
+    liste_test1 = [[(x, (x**2) - 30) for x in range(-10, 12, 1)]]
+    taille_bucket_ppm = 2
+    det = determination_des_buckets(liste_test1, taille_bucket_ppm)
     seuil = 30
 
-    assert noise_threshold(liste_test1, det_1, seuil) == [[(0, 3), (18, 21)]]
+    assert noise_threshold(liste_test1, det, seuil) == [[(0, 3), (18, 21)]]
 
 
-def test_noise_threshold_2():
-    liste_test2 = [
-        [[(i * 0.5, i * 0.5) for i in range(10)], [(i, i) for i in range(10)]]
-    ]
-
+def test_noise_threshold_2D():
+    liste_test1 = [[(i * 0.5, i * 0.5) for i in range(10)], [(i, i) for i in range(10)]]
     taille_bucket_ppm = 1
-    det_2 = determination_des_buckets(liste_test2, taille_bucket_ppm)
-    assert det_2 == [
+    det = determination_des_buckets(liste_test1, taille_bucket_ppm)
+    assert det == [
         [(0, 2), (2, 4), (4, 6), (6, 8)],
         [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)],
     ]
 
     seuil = 2
-    assert noise_threshold(liste_test2, det_2, seuil) == [
+    assert noise_threshold(liste_test1, det, seuil) == [
         [(6, 8)],
         [(3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)],
     ]
